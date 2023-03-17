cf1f7c348e6eb55a2120a63d1492b52f68d5ed4e
Achmad Dinofaldi Firmansyah <47439247+Bangik@users.noreply.github.com>
2023-03-17 21:17:18 +0800

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
+    // Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::group(['middleware' => 'admin', 'prefix' => 'admin'], function () {
+    });
+});
+
+Auth::routes();
+
