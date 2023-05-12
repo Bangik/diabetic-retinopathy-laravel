f10fde4013eef09516dcc8a9b4b217c541274841
Achmad Dinofaldi Firmansyah <47439247+Bangik@users.noreply.github.com>
2023-05-13 00:19:16 +0800

    fix dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,6 +15,12 @@ class DashboardController extends Controller
         $be_examined_room2 = Examination::where('status', 'be examined')->where('room_number', '2')->first();
         $be_examined_room3 = Examination::where('status', 'be examined')->where('room_number', '3')->first();
         $be_examined_room4 = Examination::where('status', 'be examined')->where('room_number', '4')->first();
+
+        $be_examined_room1 = $be_examined_room1 ?? Examination::where('status', 'examined')->where('room_number', '1')->first();
+        $be_examined_room2 = $be_examined_room2 ?? Examination::where('status', 'examined')->where('room_number', '2')->first();
+        $be_examined_room3 = $be_examined_room3 ?? Examination::where('status', 'examined')->where('room_number', '3')->first();
+        $be_examined_room4 = $be_examined_room4 ?? Examination::where('status', 'examined')->where('room_number', '4')->first();
+
         $be_examined_rooms12 = [$be_examined_room1, $be_examined_room2];
         $be_examined_rooms34 = [$be_examined_room3, $be_examined_room4];
 
@@ -25,7 +31,10 @@ class DashboardController extends Controller
         } else {
             $patients = Examination::where('patient_id', auth()->user()->id)->orderBy('created_at', 'desc')->get();
         }
+        
         $patients = $patients ?? [];
+        $last_queue = $last_queue ?? Examination::where('status', 'examined')->orderBy('created_at', 'desc')->first();
+        // dd($be_examined_rooms12);
         return view('dashboard', compact('patients', 'last_queue', 'be_examined_rooms12', 'be_examined_rooms34'));
     }
 }
